1aa7cfa4a8a2bf36f6ee7f42b66e597c1d93d0ca
haidaralikhan <haidaralik522@gmail.com>
2024-02-18 16:38:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,126 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  // Properties
+  const [showResults, setShowResults] = useState(false);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0);
+
+  const questions = [
+    {
+      text: "What is the capital of America?",
+      options: [
+        { id: 0, text: "New York City", isCorrect: false },
+        { id: 1, text: "Boston", isCorrect: false },
+        { id: 2, text: "Santa Fe", isCorrect: false },
+        { id: 3, text: "Washington DC", isCorrect: true },
+      ],
+    },
+    {
+      text: "When was the Indian Constitution implemented ?",
+      options: [
+        { id: 0, text: "1950", isCorrect: true },
+        { id: 1, text: "1776", isCorrect: false },
+        { id: 2, text: "1774", isCorrect: false },
+        { id: 3, text: "1826", isCorrect: false },
+      ],
+    },
+    {
+      text: "Who was the current president of the US?",
+      options: [
+        { id: 0, text: "j borden", isCorrect: true },
+        { id: 1, text: "Paul Revere", isCorrect: false },
+        { id: 2, text: "Thomas Jefferson", isCorrect: false },
+        { id: 3, text: "Benjamin Franklin", isCorrect: false },
+      ],
+    },
+    {
+      text: "What is the largest state in the india?",
+      options: [
+        { id: 0, text: "Rajasthan", isCorrect: false },
+        { id: 1, text: "U.P", isCorrect: true },
+        { id: 2, text: "bihar", isCorrect: false },
+        { id: 3, text: "M.P", isCorrect: false },
+      ],
+    },
+    {
+      text: "Who is first indian prime minister?",
+      options: [
+        { id: 0, text: "Dr.R Prashad", isCorrect: false },
+        { id: 1, text: "Modi", isCorrect: true },
+        { id: 2, text: "Rahul gandhi", isCorrect: true },
+        { id: 3, text: "JN Nehru", isCorrect: false },
+      ],
+    },
+  ];
+
+  // Helper Functions
+
+  /* A possible answer was clicked */
+  const optionClicked = (isCorrect) => {
+    // Increment the score
+    if (isCorrect) {
+      setScore(score + 1);
+    }
+
+    if (currentQuestion + 1 < questions.length) {
+      setCurrentQuestion(currentQuestion + 1);
+    } else {
+      setShowResults(true);
+    }
+  };
+
+  /* Resets the game back to default */
+  const restartGame = () => {
+    setScore(0);
+    setCurrentQuestion(0);
+    setShowResults(false);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* 1. Header  */}
+      <h1>USA Quiz 🇺🇸</h1>
+
+      {/* 2. Current Score  */}
+      <h2>Score: {score}</h2>
+
+      {/* 3. Show results or show the question game  */}
+      {showResults ? (
+        /* 4. Final Results */
+        <div className="final-results">
+          <h1>Final Results</h1>
+          <h2>
+            {score} out of {questions.length} correct - (
+            {(score / questions.length) * 100}%)
+          </h2>
+          <button onClick={() => restartGame()}>Restart game</button>
+        </div>
+      ) : (
+        /* 5. Question Card  */
+        <div className="question-card">
+          {/* Current Question  */}
+          <h2>
+            Question: {currentQuestion + 1} out of {questions.length}
+          </h2>
+          <h3 className="question-text">{questions[currentQuestion].text}</h3>
+
+          {/* List of possible answers  */}
+          <ul>
+            {questions[currentQuestion].options.map((option) => {
+              return (
+                <li
+                  key={option.id}
+                  onClick={() => optionClicked(option.isCorrect)}
+                >
+                  {option.text}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
